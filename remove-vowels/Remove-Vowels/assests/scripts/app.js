ea2c3a1aabdb6d2b6cb039ec8f1a96cbfa24acfc
Bindu-Reddy99 <80855715+Bindu-Reddy99@users.noreply.github.com>
2021-03-28 10:25:54 +0530

    Changes in remove vowels java script file

--- remove-vowels/Remove-Vowels/assests/scripts/app.js
+++ remove-vowels/Remove-Vowels/assests/scripts/app.js
@@ -3,11 +3,30 @@ const removeVowelBtn = document.getElementById("remove-vowel-btn");
 var outputInfo = document.getElementById("removedVowelrStringResult");
 function removeVowels() {
   var s = document.getElementById("inputText").value;
-  var res = s.split(" ");
-  voewlRemovedString = res.map((res) => res.replace(/[aeiou]/g, ""));
+  function removeVowels(s){
+    var vowels = "aieuo";
+    var strArr = s.toLowerCase().split("");
+    var newArr = strArr.filter(function(letter){
+        if(vowels.indexOf(letter)  == -1){
+            return letter;
+        }
+    });
+    var string = "";
+    newArr.forEach(function(letter){
+        string += letter;
+    });
+    return string;
+}
+var result=removeVowels(s);
+
+  // var res = s.split(" ");
+  // voewlRemovedString = res.map((res) => res.replace(/[aeiou]/g, ""));
+  // // voewlRemovedString+=voewlRemovedString;
+  // voewlRemovedString.join();
 
   outputInfo.innerHTML =
-    "After removing vowels in the String = " + voewlRemovedString;
+    "After removing vowels in the String = " + result;
 }
 
 removeVowelBtn.addEventListener("click", removeVowels());
+
